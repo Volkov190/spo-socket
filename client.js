38fe6775884c1242b0b04b6a89e3ba342001602b
Fedor Volkov <f.volkov10@gmail.com>
2021-10-06 21:57:48 +0300

    add websocket

--- client.js
+++ client.js
@@ -1,8 +1,38 @@
 const net = require('net');
-const express = require('express');
+const http = require('http');
+// const express = require('express');
 const path = require('path');
+const ws = require('ws');
+const fs = require('fs').promises;
+// const wss = new ws.Server({noServer: true});
+const {Server} = require('ws');
 
-const port = 2000;
+const wss = new Server({port:2000});
+
+wss.on('connection',(cli) => {
+    console.log('Client connected');
+    let mes = 0;
+    setInterval(() => cli.send(`Message: ${++mes}`), 1000);
+
+})
+
+// const server = http.createServer((req, res) => {
+//     // console.log(req.body);
+//     // res.readFile();
+//     // res.write(path.resolve(__dirname, 'front', 'index.html'))
+//     fs.readFile(path.resolve(__dirname, 'front', 'index.html')).then((cont) => res.end(cont));
+//     wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
+// });
+
+// server.listen(2000, '127.0.0.1');
+
+// function onSocketConnect(ws) {
+//     console.log('Client connected');
+// }
+
+
+
+/*const port = 2000;
 const app = express();
 const router = express.Router();
 const urlencodedParser = express.urlencoded({extended: false});
@@ -37,7 +67,7 @@ app.use(express.static(path.resolve(__dirname, 'front')));
 
 app.listen(port, () => {
     console.log('Server start...');
-});
+});*/
 
 // const client = new net.Socket();
 // client.connect(2000, '127.0.0.1', () => {
