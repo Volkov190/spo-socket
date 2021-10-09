d0da6ce9957d7bb654074253b151761681232679
Fedor Volkov <f.volkov10@gmail.com>
2021-10-09 14:08:20 +0300

    realized signin function

--- db_functions.py
+++ db_functions.py
@@ -21,7 +21,17 @@ def signup(login, password):
 
 
 def signin(login, password):
-    pass
+    result = False
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT COUNT(*) FROM public.user WHERE name=%s AND password=%s;", (login, hashlib.md5(password.encode()).hexdigest()))
+            connection.commit()
+            if (cursor.fetchone()[0] > 0):
+                result = True
+    except Exception as ex:
+        print('[DB Error] Signin Exception:', ex)
+    finally:
+        return result
 
 try:
     connection = psycopg2.connect(host = host, user = user, password = password, database = db_name)
--- server.py
+++ server.py
@@ -2,34 +2,4 @@ import socket
 import threading
 import db_functions
 
-print(db_functions.signup('Sanya', 'CXC'))
-
-# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server.bind(('127.0.0.1', 3000))
-# server.listen(4)
-
-# def listener(client_socket, x):
-#     print('new listener')
-#     try:
-#         while(True):
-#             if (client_socket):
-#                 data = client_socket.recv(1024).decode('utf-8')
-#                 print(data)
-#     except Exception as ex:
-#         print(ex)
-#         return -1
-
-# print('Ready...')
-
-# while(True):
-#     client_socket, address = server.accept()
-
-#     threading.Thread(target=listener, args=(client_socket, 0)).start()
-
-    # while(True):
-    #     if(client_socket):
-    #         data = client_socket.recv(1024).decode('utf-8')
-    #         print(data)
-    # client_socket.send(data.encode('utf-8'))
-    # client_socket.close()
-
+print(db_functions.signin('Sanya', 'CXC'))
