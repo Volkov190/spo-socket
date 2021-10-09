9716a7b5505555f8b09f930c43d884ca362ab7d4
Fedor Volkov <f.volkov10@gmail.com>
2021-10-09 19:42:02 +0300

    send messages after start by websocket

--- client.js
+++ client.js
@@ -12,80 +12,10 @@ const wss = new Server({port:2000});
 wss.on('connection',(cli) => {
     console.log('Client connected');
     let mes = 0;
-    setInterval(() => cli.send(`Message: ${++mes}`), 1000);
+    // setInterval(() => cli.send(`Message: ${++mes}`), 1000);
+    cli.send(`{"type": "start", "result": [{"text": "Test messages", "author": "Kirusha"}, {"text": "It work", "author": "Sanya"}]}`);
 
     cli.addListener('message', (message) => {
         console.log(JSON.parse(Buffer.from(message).toString()));
     });
-
 })
-
-
-
-// const server = http.createServer((req, res) => {
-//     // console.log(req.body);
-//     // res.readFile();
-//     // res.write(path.resolve(__dirname, 'front', 'index.html'))
-//     fs.readFile(path.resolve(__dirname, 'front', 'index.html')).then((cont) => res.end(cont));
-//     wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
-// });
-
-// server.listen(2000, '127.0.0.1');
-
-// function onSocketConnect(ws) {
-//     console.log('Client connected');
-// }
-
-
-
-/*const port = 2000;
-const app = express();
-const router = express.Router();
-const urlencodedParser = express.urlencoded({extended: false});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'front', 'index.html'));
-});
-
-app.post('/', urlencodedParser, (req, res) => {
-    console.log(req.body);
-
-    const client = new net.Socket();
-    client.connect(3000, '127.0.0.1', () => {
-        console.log('Connected');
-        client.write(`login=${req.body.login}&password=${req.body.password}`);
-    });
-
-    client.on('data', (data) => {
-        console.log('Recieved: ', Buffer.from(data).toString('utf-8'));
-        client.destroy();
-    });
-
-    client.on('close', () => {
-        console.log('Connection closed');
-    });
-
-    res.sendFile(path.resolve(__dirname, 'front', 'index.html'));
-});
-
-app.use('/', router);
-app.use(express.static(path.resolve(__dirname, 'front')));
-
-app.listen(port, () => {
-    console.log('Server start...');
-});*/
-
-// const client = new net.Socket();
-// client.connect(2000, '127.0.0.1', () => {
-//     console.log('Connected');
-//     client.write('Hello. Nu sho?');
-// });
-
-// client.on('data', (data) => {
-//     console.log('Recieved: ', Buffer.from(data).toString('utf-8'));
-//     client.destroy();
-// });
-
-// client.on('close', () => {
-//     console.log('Connection closed');
-// });
--- front/scripts/main.js
+++ front/scripts/main.js
@@ -1,27 +1,51 @@
 const socket = new WebSocket('ws://127.0.0.1:2000');
 const form = document.querySelector('.header__login-area');
+const signin_but = document.querySelector('.login-area__submit_sign-in');
+const signup_but = document.querySelector('.login-area__submit_sign-up');
+const script = document.querySelector('script')
 
 socket.addEventListener('message', (message) => {
     console.log(message.data);
+    message = JSON.parse(message.data);
+
+    if (message.type === 'start') {
+        for (mes of message.result) {
+            const elem = `<div class="message"><div class="message__user">${mes.author} написал(а):</div><div class="message__text">${mes.text}</div></div>`;
+            script.insertAdjacentHTML('beforebegin', elem);
+            console.log(elem);
+        }
+    }
 });
 
 socket.addEventListener('close', () => {
     console.log('Connection closed');
 });
 
-form.addEventListener('submit', (ev) => {
-    console.log(ev.target)
+signin_but.addEventListener('click', (ev) => {
     const message = {
         type: 'signin',
         login: form.login.value,
         password: form.password.value,
     };
 
-    if (form.register.value !== '') {     // @TODO переделать для регистрации
-        console.log(form.register.value);
-        message.type = 'signup';
-    }
+    socket.send(JSON.stringify(message));
+    ev.preventDefault();
+});
+
+signup_but.addEventListener('click', (ev) => {
+    const message = {
+        type: 'signup',
+        login: form.login.value,
+        password: form.password.value,
+    };
 
     socket.send(JSON.stringify(message));
     ev.preventDefault();
+});
+
+socket.addEventListener('open', () => {
+    const message = {
+        type: 'start',
+    };
+    socket.send(JSON.stringify(message))
 });
